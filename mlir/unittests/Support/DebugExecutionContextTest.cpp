88bd5409d23818a6f02ef49673e024da7442627d
Axel Villanueva <axelvillanueva@google.com>
2022-10-31 18:58:27 +0000

    Implementation of finate state machine that represent debugger flow
    First implementation, probably will clean implementation later

--- mlir/unittests/Support/DebugExecutionContextTest.cpp
+++ mlir/unittests/Support/DebugExecutionContextTest.cpp
@@ -68,7 +68,7 @@ enum DebugExecutionControl {
   Skip = 2,
   Step = 3,
   Next = 4,
-  VirtualRun = 5
+  Finish = 5
 };
 
 class DebugExecutionContext : public DebugActionManager::GenericHandler {
@@ -82,23 +82,48 @@ public:
                           ArrayRef<StringRef> instanceTags,
                           llvm::function_ref<ActionResult()> transform,
                           const DebugActionBase &action) final {
-    auto breakpoint = sbm.match(action.tag);
-    if (breakpoint) {
+    ++depth;
+    auto handleUserInput = [&]() -> bool {
       auto todoNext =
           OnBreakpoint(units, instanceTags, action.tag, action.desc);
+      while (depth == 1 && todoNext == DebugExecutionControl::Finish) {
+        todoNext = OnBreakpoint(units, instanceTags, action.tag, action.desc);
+      }
       switch (todoNext) {
       case DebugExecutionControl::Apply:
-        break;
+        depthToBreak = std::nullopt;
+        return true;
       case DebugExecutionControl::Skip:
+        depthToBreak = std::nullopt;
         return false;
-      // TODO: Support the other DebugExecutionControl
-      // Implement finate state machine that represent debugger flow
+      case DebugExecutionControl::Step:
+        depthToBreak = depth + 1;
+        return true;
+      case DebugExecutionControl::Next:
+        depthToBreak = depth;
+        return true;
+      case DebugExecutionControl::Finish:
+        depthToBreak = depth - 1;
+        return true;
       default:
-        return false;
+        assert(false);
       }
+    };
+    auto breakpoint = sbm.match(action.tag);
+    bool apply = true;
+    if (breakpoint || (depthToBreak && depth <= depthToBreak)) {
+      apply = handleUserInput();
     }
-    transform();
-    return true;
+
+    if (apply) {
+      transform();
+    }
+
+    if (depthToBreak && depth <= depthToBreak) {
+      handleUserInput();
+    }
+    --depth;
+    return apply;
   }
   SimpleBreakpoint *addSimpleBreakpoint(StringRef tag) {
     return sbm.addBreakpoint(tag);
@@ -118,6 +143,8 @@ private:
       ArrayRef<IRUnit>, ArrayRef<StringRef>, StringRef, StringRef)>
       OnBreakpoint;
   SimpleBreakpointManager &sbm;
+  int depth = 0;
+  Optional<int> depthToBreak;
 };
 
 TEST(DebugExecutionContext, DebuggerTest) {
@@ -157,6 +184,240 @@ TEST(DebugExecutionContext, DebuggerTest) {
   EXPECT_EQ(match, 2);
 }
 
+TEST(DebugExecutionContext, ApplyTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {DebuggerAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Apply};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto callback = [&]() {
+    EXPECT_EQ(counter, 1);
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  dbg->addSimpleBreakpoint(DebuggerAction::getTag());
+
+  EXPECT_TRUE(succeeded(manager.execute<DebuggerAction>({}, {}, callback)));
+  EXPECT_EQ(counter, 1);
+}
+
+TEST(DebugExecutionContext, SkipTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {DebuggerAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Skip};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto callback = [&]() {
+    assert(false);
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  dbg->addSimpleBreakpoint(DebuggerAction::getTag());
+
+  EXPECT_TRUE(failed(manager.execute<DebuggerAction>({}, {}, callback)));
+  EXPECT_EQ(counter, 1);
+}
+
+TEST(DebugExecutionContext, StepApplyTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {DebuggerAction::getTag(),
+                                        OtherAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Step, DebugExecutionControl::Apply};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto nested = [&]() {
+    EXPECT_EQ(counter, 2);
+    return noOp();
+  };
+  auto original = [&]() {
+    EXPECT_EQ(counter, 1);
+    EXPECT_TRUE(succeeded(manager.execute<OtherAction>({}, {}, nested)));
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  dbg->addSimpleBreakpoint(DebuggerAction::getTag());
+
+  EXPECT_TRUE(succeeded(manager.execute<DebuggerAction>({}, {}, original)));
+  EXPECT_EQ(counter, 2);
+}
+
+TEST(DebugExecutionContext, StepNothingInsideTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {DebuggerAction::getTag(),
+                                        DebuggerAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Step, DebugExecutionControl::Step};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto callback = [&]() {
+    EXPECT_EQ(counter, 1);
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  dbg->addSimpleBreakpoint(DebuggerAction::getTag());
+
+  EXPECT_TRUE(succeeded(manager.execute<DebuggerAction>({}, {}, callback)));
+  EXPECT_EQ(counter, 2);
+}
+
+TEST(DebugExecutionContext, NextTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {DebuggerAction::getTag(),
+                                        DebuggerAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Next, DebugExecutionControl::Next};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto callback = [&]() {
+    EXPECT_EQ(counter, 1);
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  dbg->addSimpleBreakpoint(DebuggerAction::getTag());
+
+  EXPECT_TRUE(succeeded(manager.execute<DebuggerAction>({}, {}, callback)));
+  EXPECT_EQ(counter, 2);
+}
+
+TEST(DebugExecutionContext, FinishTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {DebuggerAction::getTag(),
+                                        OtherAction::getTag(),
+                                        DebuggerAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Step, DebugExecutionControl::Finish,
+      DebugExecutionControl::Apply};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto nested = [&]() {
+    EXPECT_EQ(counter, 2);
+    return noOp();
+  };
+  auto original = [&]() {
+    EXPECT_EQ(counter, 1);
+    EXPECT_TRUE(succeeded(manager.execute<OtherAction>({}, {}, nested)));
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  auto dbgBreakpoint = dbg->addSimpleBreakpoint(DebuggerAction::getTag());
+
+  EXPECT_TRUE(succeeded(manager.execute<DebuggerAction>({}, {}, original)));
+  EXPECT_EQ(counter, 3);
+  dbg->deleteSimpleBreakpoint(dbgBreakpoint);
+}
+
+TEST(DebugExecutionContext, FinishBreakpointInNestedTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {OtherAction::getTag(),
+                                        DebuggerAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Finish, DebugExecutionControl::Apply};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto nested = [&]() {
+    EXPECT_EQ(counter, 1);
+    return noOp();
+  };
+  auto original = [&]() {
+    EXPECT_EQ(counter, 0);
+    EXPECT_TRUE(succeeded(manager.execute<OtherAction>({}, {}, nested)));
+    EXPECT_EQ(counter, 1);
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  dbg->addSimpleBreakpoint(OtherAction::getTag());
+
+  EXPECT_TRUE(succeeded(manager.execute<DebuggerAction>({}, {}, original)));
+  EXPECT_EQ(counter, 2);
+}
+
+TEST(DebugExecutionContext, FinishNothingBackTest) {
+  DebugActionManager manager;
+  std::vector<StringRef> tagSequence = {DebuggerAction::getTag(),
+                                        DebuggerAction::getTag(),
+                                        DebuggerAction::getTag()};
+  std::vector<DebugExecutionControl> controlSequence = {
+      DebugExecutionControl::Finish, DebugExecutionControl::Finish,
+      DebugExecutionControl::Apply};
+  int idx = 0, counter = 0;
+  auto onBreakpoint = [&](ArrayRef<IRUnit> units,
+                          ArrayRef<StringRef> instanceTags, StringRef tag,
+                          StringRef desc) {
+    ++counter;
+    EXPECT_EQ(tagSequence[idx], tag);
+    return controlSequence[idx++];
+  };
+  auto callback = [&]() {
+    EXPECT_EQ(counter, 3);
+    return noOp();
+  };
+  auto ptr = std::make_unique<DebugExecutionContext>(onBreakpoint);
+  auto dbg = ptr.get();
+  manager.registerActionHandler(std::move(ptr));
+  dbg->addSimpleBreakpoint(DebuggerAction::getTag());
+
+  EXPECT_TRUE(succeeded(manager.execute<DebuggerAction>({}, {}, callback)));
+  EXPECT_EQ(counter, 3);
+}
 } // namespace
 
 #endif
